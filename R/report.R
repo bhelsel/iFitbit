9e1a468116cda64bbaa7ffab71184a4368b2adfb
Brian Helsel <bhelsel@kumc.edu>
2023-07-14 10:25:27 -0500

    Change toCSV to toXLSX so SQL databases can be written to multiple Excel sheets

--- R/report.R
+++ R/report.R
@@ -1,18 +1,22 @@
+# Copyright © 2022 University of Kansas. All rights reserved.
+#
+# Creative Commons Attribution NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+
 #' @title get_fitbit_report
-#' @description Generates an HTML report, returns data, or writes to a CSV file
+#' @description Generates an HTML report, returns data, or writes to a XLSX file
 #' @param database_pathname Full pathname to the SQL database
-#' @param reports_pathname Full pathname to the location that the HTML or CSV file should be stored, Default: NULL
+#' @param reports_pathname Full pathname to the location that the HTML or XLSX file should be stored, Default: NULL
 #' @param returnData Returns the combined data set, Default: TRUE
 #' @param toHTML Renders a Rmarkdown report to HTML, Default: FALSE
-#' @param toCSV Writes the full data set to a CSV file, Default: FALSE
+#' @param toXLSX Writes the full data set to a XLSX file, Default: FALSE
 #' @param study_name A customized study name for the iFitbit report, Default: 'study'
 #' @param report_author A customized report author for the iFitbit report, Default: 'iFitbit'
 #' @param reportName PARAM_DESCRIPTION, Default: 'FitbitReport'
-#' @param start.date Add a start date to allow filtering of data when returning the data or writing it to a CSV
-#' @param end.date Add an end date to allow filtering of data when returning the data or writing it to a CSV
-#' @param ... Additional arguments passed to \code{\link[data.table]{fwrite}} or \code{\link[rmarkdown]{render}}
-#' @return Returns data set or generates a HTML or CSV file
-#' @details Generates an HTML report, returns data, or writes to a CSV file
+#' @param start.date Add a start date to allow filtering of data when returning the data or writing it to a XLSX
+#' @param end.date Add an end date to allow filtering of data when returning the data or writing it to a XLSX
+#' @param ... Additional arguments passed to \code{\link[openxlsx]{saveWorkbook}} or \code{\link[rmarkdown]{render}}
+#' @return Returns data set or generates a HTML or XLSX file
+#' @details Generates an HTML report, returns data, or writes to a XLSX file
 #' @seealso
 #'  \code{\link[DBI]{dbConnect}}, \code{\link[DBI]{dbReadTable}}, \code{\link[DBI]{dbDisconnect}}
 #'  \code{\link[RSQLite]{SQLite}}
@@ -24,11 +28,12 @@
 #' @importFrom RSQLite SQLite
 #' @importFrom rmarkdown render
 #' @importFrom data.table fwrite
+#' @importFrom openxlsx saveWorkbook writeData addWorksheet createWorkbook
 
 get_fitbit_report <- function(
     database_pathname,
     reports_pathname = NULL,
-    returnData = TRUE, toHTML = FALSE, toCSV = FALSE,
+    returnData = TRUE, toHTML = FALSE, toXLSX = FALSE,
     study_name = "study", report_author = "iFitbit",
     reportName = "FitbitReport",
     start.date,
@@ -54,21 +59,20 @@ get_fitbit_report <- function(
     rmarkdown::render(input = inputRmd, output_file = outputHTML, quiet = TRUE, ...)
   }
 
-  # Merge the data and add device attributes
-  if(toCSV | returnData){
+  # Create a list from the data
+  if(toXLSX | returnData){
     names(exercise_log)[2:ncol(exercise_log)] <- paste0("exercise_", names(exercise_log)[2:ncol(exercise_log)])
-    data <- merge(activities, merge(exercise_log, heart, by = "date", all = TRUE), by = "date", all = TRUE)
-    data %<>% dplyr::filter(date >= start.date & date <= end.date)
-    attr(data, "device") <- device
+    data <- list(device = device, activities = activities, exercise_log = exercise_log, heart = heart)
   }
 
-  if(toCSV){
-    data.table::fwrite(
-      data,
-      paste0(reports_pathname, "/", user, ".csv"),
-      dateTimeAs = "write.csv",
-      ...
-    )
+  if(toXLSX){
+    wb <- openxlsx::createWorkbook()
+    sapply(c("Device", "Activities", "Exercise Log", "Heart"), function(x) openxlsx::addWorksheet(wb, sheetName = x))
+    openxlsx::writeData(wb, sheet = "Device", x = device)
+    openxlsx::writeData(wb, sheet = "Activities", x = activities)
+    openxlsx::writeData(wb, sheet = "Exercise Log", x = exercise_log)
+    openxlsx::writeData(wb, sheet = "Heart", x = heart)
+    openxlsx::saveWorkbook(wb, paste0(reports_pathname, "/", user, ".xlsx"), overwrite = TRUE, ...)
   }
 
   if(returnData) return(data)
